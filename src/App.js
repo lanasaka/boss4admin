de04edd5229f1c240ade054a0dacd9bc5eb74c41
boss4edu <boss4edu@gmail.com>
2024-06-05 15:42:48 +0300

    update hash routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component, Suspense } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import './scss/style.scss';
 import ProtectedRoutes from './utils/protectedRoutes';
 
@@ -12,11 +12,11 @@ const loading = (
 const DefaultLayout = React.lazy(() => import('./layout/DefaultLayout'));
 const Login = React.lazy(() => import('./views/pages/login/adminLogin'));
 const Dashboard = React.lazy(() => import('./views/dashboard/Dashboard'));
-const AddUser  = React.lazy(() => import('./views/users/addUser/addUser'))
-const ShowUsers  = React.lazy(() => import('./views/users/showUsers/showUsers'))
-const NewApp  = React.lazy(() => import('./views/apps/newApp/newApp'))
-const ShowApps  = React.lazy(() => import('./views/apps/showApps/showApps'))
-const ApplicationDetails  = React.lazy(() => import('./views/apps/showApps/applicationDetail.js'))
+const AddUser = React.lazy(() => import('./views/users/addUser/addUser'))
+const ShowUsers = React.lazy(() => import('./views/users/showUsers/showUsers'))
+const NewApp = React.lazy(() => import('./views/apps/newApp/newApp'))
+const ShowApps = React.lazy(() => import('./views/apps/showApps/showApps'))
+const ApplicationDetails = React.lazy(() => import('./views/apps/showApps/applicationDetail.js'))
 
 class App extends Component {
   render() {
@@ -25,16 +25,15 @@ class App extends Component {
         <Suspense fallback={loading}>
           <Routes>
             <Route path="/" element={<Login />} />
-              <Route path="*" element={<DefaultLayout />} />
-              <Route path="*/dashboard" element={<Dashboard />} />
-              <Route path="*/users" element={<AddUser  />} />
-              <Route path="*/users/addUser" element={<AddUser  />} />
-              <Route path="*/users/showUsers" element={<ShowUsers />} />
-              <Route path="*/apps" element={<NewApp />} />
-              <Route path="*/apps/newApp" element={<NewApp />} />
-              <Route path="*/apps/showApps" element={<ShowApps />} />
-              <Route path="*/apps/:appId" element={<ApplicationDetails/>} />
-         
+            <Route path="*" element={<DefaultLayout />} />
+            <Route path="#/dashboard" element={<Dashboard />} />
+            <Route path="#/users" element={<AddUser />} />
+            <Route path="#/users/addUser" element={<AddUser />} />
+            <Route path="#/users/showUsers" element={<ShowUsers />} />
+            <Route path="/apps" element={<NewApp />} />
+            <Route path="#/apps/newApp" element={<NewApp />} />
+            <Route path="#/apps/showApps" element={<ShowApps />} />
+            <Route path="#/apps/:appId" element={<ApplicationDetails />} />
           </Routes>
         </Suspense>
       </Router>
